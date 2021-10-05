a6a8e8fe4387bab17fe6457a874dc1be051185e9
Alexey Bogomolov <mail@abogomolov.com>
2021-10-05 17:12:01 +0300

    update templates

--- try-django/trydjango/views.py
+++ try-django/trydjango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from articles.models import Article
 from random import randint
+from django.template.loader import render_to_string
 
 
 def home_view(request):
@@ -12,10 +13,13 @@ def home_view(request):
         "id": article_obj.id,
         "content": article_obj.content
     }
-    HTML_STRING = """
-    <H1>{title}</H1>
-    
-    <p>{content} with id: {id}</p>
-    """.format(**context)
+
+    # HTML_STRING = """
+    # <H1>{title}</H1>
+    #
+    # <p>{content} with id: {id}</p>
+    # """.format(**context)
+
+    HTML_STRING = render_to_string("index.html", context=context)
 
     return HttpResponse(HTML_STRING)
