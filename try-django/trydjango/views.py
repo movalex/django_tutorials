56abc99a85aa8a8c1a2294b11cb03d9b4ec93cb5
Alex Bogomolov <mail@abogomolov.com>
2021-10-06 10:40:59 +0300

    add block content

--- try-django/trydjango/views.py
+++ try-django/trydjango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from articles.models import Article
 from random import randint
+
 from django.template.loader import render_to_string
 from django.template.loader import get_template  # another way to do the same
 
@@ -14,17 +15,16 @@ def home_view(request):
         "id": article_obj.id,
         "content": article_obj.content
     }
-    # bare strings:
+    # use strings:
 
     # HTML_STRING = """
     # <H1>{title}</H1>
-    #
     # <p>{content} with id: {id}</p>
     # """.format(**context)
 
     html_string = render_to_string("index.html", context=context)
 
-    # another approach
+    # another approach:
 
     # tmpl = get_template("index.html")
     # html_string = tmpl.render(context=context)
